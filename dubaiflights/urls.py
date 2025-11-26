1ebf61dd4cd44f782b73e2e5b2d17e37849456f5
syntaco-zak <zak@syntaco.uk>
2025-11-26 16:59:11 +0000

    redirected homepage link to flights

--- dubaiflights/urls.py
+++ dubaiflights/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/flights/', permanent=False)),
     path("flights/", include("flights.urls")),
     path("admin/", admin.site.urls),
 ]
