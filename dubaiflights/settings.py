455efc1eea724f0e34e8e9c6b0208ab125f43990
syntaco-zak <zak@syntaco.uk>
2025-11-26 16:43:54 +0000

    added render domain to hosts

--- dubaiflights/settings.py
+++ dubaiflights/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-zr^g+lxt4(#c*-ax9%nmz$cno0v1^6ofthh289om(m$i2c6u0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "dubaiflights.onrender.com",
+]
 
 
 # Application definition
